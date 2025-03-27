cda9d2695a5ebd0ad9fc22b709cc7361c8407d2e
SAHIL DHIMAN <121869766+Sahil-Dhiman-creator@users.noreply.github.com>
2025-03-27 14:48:24 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*****************************TYPING ANIMATION*******************************/
+var typed = new Typed(".typing", {
+    strings:
+    [
+        "",
+        "Web Developer",
+        "Web Designer",
+        "Frontend Developer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+});
+/*****************************TYPING ANIMATION IN ABOUT-SECTION*******************************/
+var typed = new Typed(".type", {
+    strings:
+    [
+        "Web Developer",
+        "Web Designer",
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
